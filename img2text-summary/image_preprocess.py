036fb4d93a9565cb0834811f9fc1bab6706b56f2
Kapilesh-H <136972245+Kapilesh-H@users.noreply.github.com>
2024-04-14 11:22:33 +0530

    image_preprocess.py

--- img2text-summary/image_preprocess.py
+++ img2text-summary/image_preprocess.py
@@ -10,3 +10,23 @@ Functionalities:
   d. Resize the image
   e. Convert it into binary image
 '''
+from PIL import Image
+import numpy as np
+image = Image.open(file_path)//to be replaced
+def grayscale(image):
+  greyscaled_img=np.average(image, axis=2)
+  return greyscaled_img
+def resize(image, new_shape):
+  height, width = new_shape
+  resized_img=image[:height, :width]
+  return resized_img
+def normalize(image):
+  normalized_img=image.astype(np.float32) / 255.0
+  return normalized_img
+def preprocess_image(image, target_shape=(224, 224)):
+  image = grayscale(image)
+  image = resize(image, target_shape)
+  image = normalize(image)
+  if len(image.shape) == 2:
+    image = np.expand_dims(image, axis=-1)
+  return image
